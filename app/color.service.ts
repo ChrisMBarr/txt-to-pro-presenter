a8a99d0051788313e47b9c5e69b77271b346e39b
Chris Barr <chris.m.barr@gmail.com>
2016-03-23 21:05:31 -0400

    More robust color conversions

--- app/color.service.ts
+++ app/color.service.ts
@@ -1,7 +1,7 @@
 /// <reference path="app-typings.d.ts" />
 namespace TxtToPp.Services {
     'use strict';
-    
+
     export class ColorService {
 
         //constructor() { }
@@ -10,11 +10,18 @@ namespace TxtToPp.Services {
             hex = hex.replace('#', '');
             return {
                 /* tslint:disable: object-literal-sort-keys */
-                r: parseInt(hex.substring(0, 2), 16),
-                g: parseInt(hex.substring(2, 4), 16),
-                b: parseInt(hex.substring(4, 6), 16)
+                r: (parseInt(hex.substring(0, 2), 16) || 0),
+                g: (parseInt(hex.substring(2, 4), 16) || 0),
+                b: (parseInt(hex.substring(4, 6), 16) || 0)
                 /* tslint:enable: object-literal-sort-keys */
             };
+        };
+
+        public rgbToHexColor = (rgbColor: Interfaces.IRgbColor): string => {
+            let r = ("0" + rgbColor.r.toString(16)).slice(-2);
+            let g = ("0" + rgbColor.g.toString(16)).slice(-2);
+            let b = ("0" + rgbColor.b.toString(16)).slice(-2);
+            return `#${r}${g}${b}`;
         };
     }
 
